34f1d783509385cf32d02fbb762d23225599b8b7
ewenja <ewe49032@gmail.com>
2025-02-15 23:49:50 +0800

    Update HihiHub.lua

--- HihiHub.lua
+++ HihiHub.lua
@@ -14,11 +14,11 @@ local function getHttpRequest()
             else
                 error("AWP 不支持 request 或 http_request")
             end
-        elseif executor:find("sonar") then -- 適配 Sonar (Velocity)
+        elseif executor:find("Sonar 0.2.0") then -- 適配 Sonar (Velocity)
             if request then
-                return request, "Velocity (Sonar)"
+                return request, "Sonar 0.2.0"
             elseif http_request then
-                return http_request, "Velocity (Sonar)"
+                return http_request, "Sonar 0.2.0"
             else
                 error("Velocity (Sonar) 不支持 request 或 http_request")
             end
