b2ff0db8d728366f48fc66825d2f026fbec2b5b7
ewenja <ewe49032@gmail.com>
2025-05-31 21:07:24 +0800

    Update BT.lua

--- BT.lua
+++ BT.lua
@@ -2,16 +2,17 @@
 print("Executor Name:", getexecutorname and getexecutorname() or "未知執行器")
 
 -- 嘗試獲取 HTTP 請求函數與執行器名稱
-local function getHttpRequest()
-    if syn and syn.request then
-        return syn.request, "synapse z"
-    elseif request then
-        return request, "velocity"
-    elseif http_request then
-        return http_request, "velocity"
-    elseif fluxus and fluxus.request then
-        return fluxus.request, "fluxus"
-    end
+if syn and syn.request then
+    return syn.request, "synapse z"
+elseif getexecutorname and getexecutorname():lower():find("visual") and http_request then
+    return http_request, "visual"
+elseif fluxus and fluxus.request then
+    return fluxus.request, "fluxus"
+elseif request then
+    return request, "velocity"
+elseif http_request then
+    return http_request, "velocity"
+end
     error("未偵測到支援的 HTTP 請求函數，請使用 Synapse、Fluxus 或 Velocity。")
 end
 
@@ -21,7 +22,7 @@ local httpRequest, executorName = getHttpRequest()
 -- 向伺服器發送請求以獲取腳本
 local success, response = pcall(function()
     return httpRequest({
-        Url = "https://haihai.hihihub.workers.dev/",
+        Url = "https://ewe.hihihub.workers.dev/",
         Method = "GET",
         Headers = {
             ["User-Agent"] = executorName,
